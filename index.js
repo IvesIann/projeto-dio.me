ca4cedf4629a4688696f8c376c1c27e2ffd215fa
IvesIann <ives.ian@hotmail.com>
2023-10-24 23:38:06 -0300

    Codigo Pronto

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Teste de Projeto")
+let nomeHeroi = "Ives"
+let nivelHeroi = 100000
+
+if(nivelHeroi < 1000){
+    nivelHeroi = "Ferro"
+    console.log("O Herói de nome " + nomeHeroi+ " está no nível: " + nivelHeroi)   
+    
+}else if(nivelHeroi >= 1001 && nivelHeroi <= 2000){
+    nivelHeroi = "Bronze"
+    console.log("O Herói de nome " + nomeHeroi+ " está no nível: " + nivelHeroi)    
+
+}else if(nivelHeroi >= 2001 && nivelHeroi <= 5000){
+    nivelHeroi = "Prata"
+    console.log("O Herói de nome " + nomeHeroi+ "está no nível: " + nivelHeroi)    
+
+}else if(nivelHeroi >= 5001 && nivelHeroi <= 7000){
+    nivelHeroi = "Ouro"
+    console.log("O Herói de nome " + nomeHeroi+ " está no nível: " + nivelHeroi)    
+
+}else if(nivelHeroi >= 7001 && nivelHeroi <= 8000){
+    nivelHeroi = "Platina"
+    console.log("O Herói de nome " + nomeHeroi+ " está no nível: " + nivelHeroi)    
+
+}else if(nivelHeroi >= 8001 && nivelHeroi <= 9000){
+    nivelHeroi = "Ascendente"
+    console.log("O Herói de nome " + nomeHeroi+ " está no nível: " + nivelHeroi)    
+
+}else if(nivelHeroi >= 9001 && nivelHeroi <= 10000){
+    nivelHeroi = "Imortal"
+    console.log("O Herói de nome " + nomeHeroi+ " está no nível: " + nivelHeroi)    
+
+}else{
+    nivelHeroi = "Radiante"
+    console.log("O Herói de nome " + nomeHeroi+ " está no nível: " + nivelHeroi) 
+}  
